daae67c948a265f0f9a3a1df3cebc1634cd65d35
Guilherme-Boves <ghboves@gmail.com>
2022-03-05 15:19:27 -0300

    Padrões de Projetos com Java

---
+++ src/Facade/FacadeApp.java
@@ -0,0 +1,11 @@
+package Facade;
+
+public class FacadeApp {
+
+	public static void main(String[] args) {
+		
+		Facade facade = new Facade();
+		
+		facade.migrarCliente("Guilherme", "18965410");
+	}
+}
---
+++ src/Strategy/StrategyApp.java
@@ -0,0 +1,31 @@
+package Strategy;
+
+public class StrategyApp {
+
+	public static void main(String[] args) {
+
+		// Strategy
+		
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		
+		Robo robo = new Robo();
+		robo.setComportamento(normal);		
+		robo.mover();
+		robo.mover();
+		
+		System.out.println();
+		robo.setComportamento(defensivo);		
+		robo.mover();
+		
+		System.out.println();
+		robo.setComportamento(agressivo);		
+		robo.mover();
+		robo.mover();
+		robo.mover();
+		
+		
+	}
+
+}
